1e6037b10e1d6c8ccd46917609b58b30361c737f
Jbasilio55 <j.l.basilio55@gmail.com>
2023-05-06 22:48:44 -0400

    start of back-end portion of Movies-app

---
+++ Back-end/movies/src/test/java/tech/jorgebasilio/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.jorgebasilio.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
